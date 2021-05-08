8c14e3a8198e001c64a617d455dc7313086a07bf
Ali Arslan Ansari <aliarslan1620@gmail.com>
2021-05-08 23:17:37 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,71 @@
+import { Text } from "@chakra-ui/react"
+import React from "react"
+
+import "./App.css"
+import Header from "./components/Header"
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <Text variant="h1">hello</Text>
+      <h1>hello</h1>
+      <h1>hello</h1>
+      <h1>hello</h1>
+    </div>
+  )
+}
+
+export default App
